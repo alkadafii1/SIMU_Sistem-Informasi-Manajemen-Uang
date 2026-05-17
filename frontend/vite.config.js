609b6af52ed242b0bd13889348a95c692e3d9b10
Celvv1 <calvinsobo@gmail.com>
2026-05-17 20:04:26 +0700

    [+] frontend : Menambahkan tailwind,react ke vite.config.js agar code bekerja dengan lancar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss' // Menggunakan paket v4 baru
+import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
 })
