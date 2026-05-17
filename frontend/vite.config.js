8491cb7276f098d13b5330b3a51f32391f3ca977
flutter <alkadafifirnawan1@gmail.com>
2026-05-17 21:00:21 +0700

    [+] feat: setup deployment workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/alkadafii1/SIMU_Sistem-Informasi-Manajemen-Uang.git',
   plugins: [react()],
   css: {
     postcss: {
@@ -13,5 +14,6 @@ export default defineConfig({
         autoprefixer(),
       ],
     },
+   
   },
 })
