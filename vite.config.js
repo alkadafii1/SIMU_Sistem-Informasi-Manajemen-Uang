36cf8644ec66fee9297e255f6ab89e9800557d68
flutter <alkadafifirnawan1@gmail.com>
2026-05-18 09:30:31 +0700

    [~] fix: memperbaiki deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/SIMU_Sistem-Informasi-Manajemen-Uang/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 })
