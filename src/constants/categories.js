631e27be5cd8b9c008a14c29c386467db7659b6b
flutter <alkadafifirnawan1@gmail.com>
2026-05-29 10:19:19 +0700

    [+] feat: Add constants categories

---
+++ src/constants/categories.js
@@ -0,0 +1,29 @@
+// Kategori untuk Kebutuhan (Needs)
+export const NEEDS_CATEGORIES = [
+  'Makanan & Minuman',
+  'Belanja Harian',
+  'Transportasi',
+  'Tagihan & Utilitas',
+  'Kesehatan',
+  'Pendidikan',
+  'Sewa',
+  'Cicilan'
+];
+
+// Kategori untuk Keinginan (Wants)
+export const WANTS_CATEGORIES = [
+  'Hiburan & Hobi',
+  'Makan di Luar',
+  'Belanja',
+  'Olahraga',
+  'Lainnya'
+];
+
+// Semua kategori
+export const ALL_CATEGORIES = [...NEEDS_CATEGORIES, ...WANTS_CATEGORIES];
+
+// Warna untuk donut chart
+export const DONUT_COLORS = ['#006D77', '#2A9D8F', '#80CED7'];
+
+// Warna untuk kategori
+export const CATEGORY_COLORS = ['#006D77', '#2A9D8F', '#80CED7', '#F4A261', '#E76F51'];
